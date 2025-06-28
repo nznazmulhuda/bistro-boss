d0b52b5a7d5939b00c823357b51b01389914cb33
Nazmul Huda <nznazmulhuda@users.noreply.github.com>
2025-06-28 13:12:03 +0600

    call us section done✅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Banner from "@/components/pages/home/banner/banner";
 import CallInAction from "@/components/pages/home/callInAction/callInAction";
+import CallUs from "@/components/pages/home/callUs/callUs";
 import Menu from "@/components/pages/home/menu/menu";
 import OrderSlider from "@/components/pages/home/order/order";
 import SectionTitle from "@/components/shared/sectionTitle/sectionTitle";
@@ -21,6 +22,8 @@ export default function Homepage() {
       <SectionTitle title="FROM OUR MENU" semiTitle="---Check it out---" />
 
       <Menu />
+
+      <CallUs />
     </>
   );
 }
---
+++ src/components/pages/home/callUs/callUs.tsx
@@ -0,0 +1,11 @@
+export default function CallUs() {
+  return (
+    <section className="container mx-auto mt-[130px] mb-[50px] bg-[#151515]">
+      <main className="flex items-center justify-center py-24">
+        <h5 className="font-raleway text-center text-[50px] font-semibold text-white">
+          Call Us: +88 019234567891
+        </h5>
+      </main>
+    </section>
+  );
+}
